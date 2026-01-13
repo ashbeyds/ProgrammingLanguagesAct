02d3194cc9c5c68772da991d5594405de0135c35
ashbeyds <delossantos.ashbeybscs2024@gmail.com>
2026-01-14 01:03:21 +0800

    big changes/ added laman

--- PL_Act2/PL_Act2/Form1.Designer.cs
+++ PL_Act2/PL_Act2/Form1.Designer.cs
@@ -28,13 +28,262 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FName = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.GradeLvl = new System.Windows.Forms.ComboBox();
+            this.Age = new System.Windows.Forms.TextBox();
+            this.Absences = new System.Windows.Forms.TextBox();
+            this.Average = new System.Windows.Forms.TextBox();
+            this.Payment = new System.Windows.Forms.ComboBox();
+            this.DisciplinaryMethod = new System.Windows.Forms.ComboBox();
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Clearance = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // FName
+            // 
+            this.FName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FName.Location = new System.Drawing.Point(17, 59);
+            this.FName.Name = "FName";
+            this.FName.Size = new System.Drawing.Size(325, 26);
+            this.FName.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.panel1.Location = new System.Drawing.Point(361, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(433, 442);
+            this.panel1.TabIndex = 1;
+            // 
+            // GradeLvl
+            // 
+            this.GradeLvl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GradeLvl.FormattingEnabled = true;
+            this.GradeLvl.Items.AddRange(new object[] {
+            "Grade 7",
+            "Grade 8",
+            "Grade 9",
+            "Grade 10",
+            "Grade 11",
+            "Grade 12"});
+            this.GradeLvl.Location = new System.Drawing.Point(188, 132);
+            this.GradeLvl.Name = "GradeLvl";
+            this.GradeLvl.Size = new System.Drawing.Size(154, 28);
+            this.GradeLvl.TabIndex = 2;
+            // 
+            // Age
+            // 
+            this.Age.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Age.Location = new System.Drawing.Point(18, 132);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(155, 26);
+            this.Age.TabIndex = 3;
+            // 
+            // Absences
+            // 
+            this.Absences.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Absences.Location = new System.Drawing.Point(189, 205);
+            this.Absences.Name = "Absences";
+            this.Absences.Size = new System.Drawing.Size(154, 26);
+            this.Absences.TabIndex = 4;
+            // 
+            // Average
+            // 
+            this.Average.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Average.Location = new System.Drawing.Point(17, 205);
+            this.Average.Name = "Average";
+            this.Average.Size = new System.Drawing.Size(154, 26);
+            this.Average.TabIndex = 5;
+            // 
+            // Payment
+            // 
+            this.Payment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Payment.FormattingEnabled = true;
+            this.Payment.Items.AddRange(new object[] {
+            "Paid",
+            "Unpaid"});
+            this.Payment.Location = new System.Drawing.Point(188, 351);
+            this.Payment.Name = "Payment";
+            this.Payment.Size = new System.Drawing.Size(154, 28);
+            this.Payment.TabIndex = 7;
+            // 
+            // DisciplinaryMethod
+            // 
+            this.DisciplinaryMethod.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisciplinaryMethod.FormattingEnabled = true;
+            this.DisciplinaryMethod.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.DisciplinaryMethod.Location = new System.Drawing.Point(17, 351);
+            this.DisciplinaryMethod.Name = "DisciplinaryMethod";
+            this.DisciplinaryMethod.Size = new System.Drawing.Size(154, 28);
+            this.DisciplinaryMethod.TabIndex = 8;
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Location = new System.Drawing.Point(121, 402);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(105, 36);
+            this.SubmitBtn.TabIndex = 0;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Full Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(185, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Grade Level";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Average";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(185, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Absences";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(14, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Clearance";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(14, 328);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(165, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Disciplinary Method";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(185, 328);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Payment";
+            // 
+            // Clearance
+            // 
+            this.Clearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clearance.FormattingEnabled = true;
+            this.Clearance.Items.AddRange(new object[] {
+            "Academic",
+            "Library",
+            "Full"});
+            this.Clearance.Location = new System.Drawing.Point(17, 277);
+            this.Clearance.Name = "Clearance";
+            this.Clearance.Size = new System.Drawing.Size(325, 28);
+            this.Clearance.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.Clearance);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SubmitBtn);
+            this.Controls.Add(this.DisciplinaryMethod);
+            this.Controls.Add(this.Payment);
+            this.Controls.Add(this.Average);
+            this.Controls.Add(this.Absences);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.GradeLvl);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.FName);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Activity 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox FName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox GradeLvl;
+        private System.Windows.Forms.TextBox Age;
+        private System.Windows.Forms.TextBox Absences;
+        private System.Windows.Forms.TextBox Average;
+        private System.Windows.Forms.ComboBox Payment;
+        private System.Windows.Forms.ComboBox DisciplinaryMethod;
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox Clearance;
     }
 }
 
--- PL_Act2/PL_Act2/Form1.cs
+++ PL_Act2/PL_Act2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,73 @@ namespace PL_Act2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            int age, gradeLvl, average, absences;
+            string academicStanding, attendanceStatus, privilegeLvl, finalRisk;
+
+            if (string.IsNullOrEmpty(FName.Text) ||
+                string.IsNullOrEmpty(Age.Text) ||
+                string.IsNullOrEmpty(Average.Text) ||
+                string.IsNullOrEmpty(Absences.Text) ||
+                Clearance.SelectedIndex == -1 ||
+                GradeLvl.SelectedIndex == -1 ||
+                DisciplinaryMethod.SelectedIndex == -1 ||
+                Payment.SelectedIndex == -1) {
+                MessageBox.Show("Please fill out all fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            else if (!int.TryParse(Age.Text, out age)) {
+                MessageBox.Show("Invalid Age", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            else if (!int.TryParse(Average.Text, out average)) {
+                MessageBox.Show("Invalid Average", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            else if (!int.TryParse(Absences.Text, out absences)) {
+                MessageBox.Show("Invalid Absences.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            gradeLvl = GradeLvl.SelectedIndex + 7;
+            if ((age < 12 && gradeLvl >= 10) || (age >= 18 && gradeLvl <= 8)) {
+                MessageBox.Show("Invalid Record", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (average >= 90) {
+                academicStanding = "Outstanding";
+            }
+            else if (average >= 85) {
+                academicStanding = "Very Satisfactory";
+            }
+            else if (average >= 75) {
+                academicStanding = "Satisfactory";
+            }
+            else
+                academicStanding = "At Risk";
+
+            if (absences >= 3) {
+                 attendanceStatus = "Good Standing";
+            }
+            else if (absences <= 7) {
+                attendanceStatus = "Warning";
+            }
+            else {
+                attendanceStatus = "Critical";  
+            }
+
+
+                MessageBox.Show($"Name:\t{FName.Text} \n" +
+                                $"Academic Standing:\t{academicStanding}\n" +
+                                $"Attendance Status:\t{attendanceStatus}\n+" +
+                                $"Clearance Result:\t{}"); 
         }
     }
 }
